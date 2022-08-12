e59b2d7036d551f503f0dc86083c8f3dde41ac45
FallicoFunctions <nickf579@gmail.com>
2022-08-12 00:33:35 -0700

    Updated LoginController

--- src/controller/LoginController.java
+++ src/controller/LoginController.java
@@ -7,7 +7,6 @@ import java.net.URL;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -44,18 +43,18 @@ import util.DateTime;
  */
 public class LoginController implements Initializable {
     @FXML
-    private Label utcLabel;
+    public Label utcLabel;
     @FXML
-    private TextField loginEntryBox;
+    public TextField loginEntryBox;
     @FXML
-    private PasswordField passwordEntryBox;
+    public PasswordField passwordEntryBox;
     @FXML
-    private Button submitButton;
+    public Button submitButton;
     @FXML
-    private Label titleLabel;
-    ObservableList<Appointment> appointmentReminderOL = FXCollections.observableArrayList();
-    private DateTimeFormatter datetimeDTF = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
-    private ZoneId localTime = ZoneId.systemDefault();
+    public Label titleLabel;
+    ObservableList<Appointment> upcomingAppList = FXCollections.observableArrayList();
+    public DateTimeFormatter formatterVariable = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm");
+    public ZoneId localTime = ZoneId.systemDefault();
     ResourceBundle rb = ResourceBundle.getBundle("properties.login", Locale.getDefault());
 
     /** Initializes the controller class.
@@ -65,7 +64,6 @@ public class LoginController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         System.out.println("Before rb");
-
         try {
             rb = ResourceBundle.getBundle("properties.login", Locale.getDefault());
             System.out.println("After rb");
@@ -74,152 +72,134 @@ public class LoginController implements Initializable {
             passwordEntryBox.setPromptText(rb.getString("password"));
             submitButton.setText(rb.getString("signin"));
             utcLabel.setText(ZoneId.systemDefault().toString());
-        } catch (MissingResourceException e) {
+        } catch (MissingResourceException exception) {
             System.out.println("Missing resource");
+            exception.printStackTrace();
         }
     }
 
     /** This method filters the reminder list and alerts if appointment is within 15 minutes.
      * <p>
-     * The lambda in this method will identify any appointments starting in the next 15 minutes. It is justified because
-     * the code is more efficient, and therefore the program is more efficient.
+     * The lambda on line 94 will locate appointments starting within a quarter of an hour from now. It is justified
+     * because the code is more efficient, and therefore the program is more efficient.
      * </p>
      * */
-    private void appointmentAlert() {
-        System.out.println("**** Being appointmentAlert ****");
-
-        LocalDateTime now = LocalDateTime.now();
-        LocalDateTime nowPlus15Min = now.plusMinutes(15);
-        System.out.println("Now: " + now);
-        System.out.println("NowPlus15: " + nowPlus15Min);
-
-        FilteredList<Appointment> filteredData = new FilteredList<>(appointmentReminderOL);
-
-        //lambda expression used to efficiently identify any appointment starting within the next 15 minutes
-        filteredData.setPredicate(row -> {
-                    LocalDateTime rowDate = LocalDateTime.parse(row.getStart().substring(0, 16), datetimeDTF);
-                    return rowDate.isAfter(now.minusMinutes(1)) && rowDate.isBefore(nowPlus15Min);
+    public void upcomingAlertSignal() {
+        System.out.println("**** Being upcomingAlertSignal ****");
+        LocalDateTime current = LocalDateTime.now();
+        LocalDateTime soon = current.plusMinutes(15);
+        System.out.println("Now: " + current);
+        System.out.println("NowPlus15: " + soon);
+        FilteredList<Appointment> sortedList = new FilteredList<>(upcomingAppList);
+        sortedList.setPredicate(row -> {
+                    LocalDateTime appointmentTime = LocalDateTime.parse(row.getStart().substring(0, 16), formatterVariable);
+                    return appointmentTime.isAfter(current.minusMinutes(1)) && appointmentTime.isBefore(soon);
                 }
         );
-        if (filteredData.isEmpty()) {
+        if (sortedList.isEmpty()) {
             System.out.println("No upcoming appointment alerts.");
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle(rb.getString("noappointment"));
-            alert.setHeaderText(rb.getString("noappointment15"));
-            alert.setContentText(rb.getString("clickokay"));
-            alert.showAndWait();
+            Alert signal = new Alert(Alert.AlertType.INFORMATION);
+            signal.setTitle(rb.getString("noappointment"));
+            signal.setHeaderText(rb.getString("noappointment15"));
+            signal.setContentText(rb.getString("clickokay"));
+            signal.showAndWait();
         } else {
-            String type = filteredData.get(0).getDescription();
-            String customer = filteredData.get(0).getCustomerName();
-            String start = filteredData.get(0).getStart().substring(0, 16);
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle(rb.getString("upcomingappointment"));
-            alert.setHeaderText(rb.getString("appointmentreminder"));
-            alert.setContentText(rb.getString("appointmentwith1") + customer
+            String type = sortedList.get(0).getDescription();
+            String customer = sortedList.get(0).getCustomerName();
+            String start = sortedList.get(0).getStart().substring(0, 16);
+            Alert signal = new Alert(Alert.AlertType.INFORMATION);
+            signal.setTitle(rb.getString("upcomingappointment"));
+            signal.setHeaderText(rb.getString("appointmentreminder"));
+            signal.setContentText(rb.getString("appointmentwith1") + customer
                     + rb.getString("appointmentwith2") + start + ".");
-            alert.showAndWait();
+            signal.showAndWait();
         }
-        System.out.println("**** End appointmentAlert ****");
+        System.out.println("**** End upcomingAlertSignal ****");
     }
 
     /** This method creates reminder list to be checked with the appointment Alert. */
-    private void createReminderList() {
-        System.out.println("**** Begin createReminderList ****");
+    public void selectUpcomingApps() {
+        System.out.println("**** Begin selectUpcomingApps ****");
         System.out.println(User.getUsername());
         try {
-            PreparedStatement ps = ConnectDB.makeConnection().prepareStatement(
-                    "SELECT appointments.appointment_Id, appointments.customer_Id, appointments.title, appointments.description, "
-                            + "appointments.`start`, appointments.`end`, customers.customer_Id, customers.customer_Name, appointments.created_By "
-                            + "FROM appointments, customers "
-                            + "WHERE appointments.customer_Id = customers.customer_Id AND appointments.created_By = ? "
+            PreparedStatement sql = ConnectDB.makeConnection().prepareStatement(
+                    "SELECT appointments.appointment_Id, appointments.customer_Id, appointments.title, " +
+                            "appointments.description, appointments.`start`, appointments.`end`, customers.customer_Id," +
+                            " customers.customer_Name, appointments.created_By FROM appointments, customers WHERE " +
+                            "appointments.customer_Id = customers.customer_Id AND appointments.created_By = ? "
                             + "ORDER BY `start`");
-            ps.setString(1, User.getUsername());
-            ResultSet rs = ps.executeQuery();
+            sql.setString(1, User.getUsername());
+            ResultSet set = sql.executeQuery();
+            while (set.next()) {
+                Timestamp currentStart = set.getTimestamp("start");
+                ZonedDateTime universalStart = currentStart.toLocalDateTime().atZone(ZoneId.of("UTC"));
+                ZonedDateTime userTimeStart = universalStart.withZoneSameInstant(localTime);
+                Timestamp currentEnd = set.getTimestamp("end");
+                ZonedDateTime universalEnd = currentEnd.toLocalDateTime().atZone(ZoneId.of("UTC"));
+                ZonedDateTime userTimeEnd = universalEnd.withZoneSameInstant(localTime);
+                int appNumber = set.getInt("appointment_Id");
+                String appTitle = set.getString("title");
+                String description = set.getString("description");
+                String custName = set.getString("customer_Name");
+                int custID = set.getInt("customer_Id");
+                String creator = set.getString("created_By");
+                upcomingAppList.add(new Appointment(appNumber, userTimeStart.toString(), userTimeEnd.toString(), appTitle, description, custID, custName, creator));
 
-            while (rs.next()) {
-                //pulls start time from database and converts it into local time zone
-                Timestamp timestampStart = rs.getTimestamp("start");
-                ZonedDateTime startUTC = timestampStart.toLocalDateTime().atZone(ZoneId.of("UTC"));
-                ZonedDateTime newLocalStart = startUTC.withZoneSameInstant(localTime);
-
-                //pulls end time from database and converts it into local time zone
-                Timestamp timestampEnd = rs.getTimestamp("end");
-                ZonedDateTime endUTC = timestampEnd.toLocalDateTime().atZone(ZoneId.of("UTC"));
-                ZonedDateTime newLocalEnd = endUTC.withZoneSameInstant(localTime);
-
-                //pulls select data fields for use in appointmentReminderOL observablelist
-                int appointmentId = rs.getInt("appointment_Id");
-                String title = rs.getString("title");
-                String type = rs.getString("description");
-                String customerName = rs.getString("customer_Name");
-                int customerId = rs.getInt("customer_Id");
-                String user = rs.getString("created_By");
-
-                //prints values of data fields prior to being inserted into observablelist
-                System.out.println("AppointmentID: " + appointmentId);
-                System.out.println("newLocalStart: " + newLocalStart.toString());
-                System.out.println("newLocalEnd: " + newLocalEnd.toString());
-                System.out.println("Title: " + title);
-                System.out.println("Type: " + type);
-                System.out.println("CustomerId: " + customerId);
-                System.out.println("CustomerName: " + customerName);
-                System.out.println("User: " + user);
-
-                //inserts Appointment objects into observablelist
-                appointmentReminderOL.add(new Appointment(appointmentId, newLocalStart.toString(), newLocalEnd.toString(), title, type, customerId, customerName, user));
+                System.out.println("appNumber: " + appNumber);
+                System.out.println("userTimeStart: " + userTimeStart.toString());
+                System.out.println("userTimeEnd: " + userTimeEnd.toString());
+                System.out.println("Title: " + appTitle);
+                System.out.println("description: " + description);
+                System.out.println("custID: " + custID);
+                System.out.println("custName: " + custName);
+                System.out.println("creator: " + creator);
             }
-
-        } catch (SQLException sqe) {
+        } catch (SQLException sql) {
             System.out.println("There is an error in your SQL preparedstatement");
-            sqe.printStackTrace();
-        } catch (Exception e) {
+            sql.printStackTrace();
+        } catch (Exception exception) {
             System.out.println("An error other than your SQL has occurred.");
-            e.printStackTrace();
+            exception.printStackTrace();
         }
         System.out.println("**** End create Reminder List ****");
     }
 
-    /** This method handles the login button.
+    /** This method handles the submit button.
      * @param event When actioned checks if the login is valid and if so loads the main screen. */
     @FXML
-    private void submitButtonHandler(ActionEvent event) throws SQLException, IOException {
-        String usernameInput = loginEntryBox.getText();
-        String passwordInput = passwordEntryBox.getText();
-        int userID = getUserID(usernameInput);
+    public void submitButtonHandler(ActionEvent event) throws SQLException, IOException {
+        String loginCredential = loginEntryBox.getText();
+        String passCredential = passwordEntryBox.getText();
+        int creatorID = getCreatorNumber(loginCredential);
         ResourceBundle rb = ResourceBundle.getBundle("properties.login", Locale.getDefault());
         Parent parent;
         Stage setup;
         User user = new User();
-
-        if (isValidPassword(userID, passwordInput)) {
-            User.setUserID(userID);
-            User.setUsername(usernameInput);
-
-            //calls method to write current user to the log
-            loginLog(User.getUsername());
-            createReminderList();
-            appointmentAlert();
-
-            //calls mainscreen scene after successful login
+        if (passwordChecker(creatorID, passCredential)) {
+            User.setUserID(creatorID);
+            User.setUsername(loginCredential);
+            loginActivity(User.getUsername());
+            selectUpcomingApps();
+            upcomingAlertSignal();
             parent = FXMLLoader.load(getClass().getResource("/view/MainScreen.fxml"));
             setup = (Stage) submitButton.getScene().getWindow();
             Scene scene = new Scene(parent);
             setup.setScene(scene);
             setup.show();
         } else {
-            loginLogFail();
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("");
-            alert.setHeaderText(rb.getString("loginalertheader"));
-            alert.setContentText(rb.getString("loginalertcontent"));
-            Optional<ButtonType> result = alert.showAndWait();
+            failedLoginActivity();
+            Alert signal = new Alert(Alert.AlertType.INFORMATION);
+            signal.setTitle("");
+            signal.setHeaderText(rb.getString("loginalertheader"));
+            signal.setContentText(rb.getString("loginalertcontent"));
+            Optional<ButtonType> result = signal.showAndWait();
         }
     }
 
-    /** This method creates a new log file if one doesn't exist and inserts successful login information for current user.
+    /** This method records each successful login attempt and places them on the file login_activity.txt. 
      * @param user The username of the person logging in.
      * */
-    public void loginLog(String user) {
+    public void loginActivity(String user) {
         try {
             String fileName = "login_activity";
             BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));
@@ -227,13 +207,13 @@ public class LoginController implements Initializable {
             System.out.println("New login recorded in log file.");
             writer.flush();
             writer.close();
-        } catch (IOException e) {
-            System.out.println(e);
+        } catch (IOException exception) {
+            exception.printStackTrace();
         }
     }
 
-    /** This method creates a new log file if one doesn't exist and inserts failed login information for current user. */
-    public void loginLogFail() {
+    /** This method records each failed login attempt and places them on the file login_activity.txt. */
+    public void failedLoginActivity() {
         try {
             String fileName = "login_activity";
             BufferedWriter writer = new BufferedWriter(new FileWriter(fileName, true));
@@ -241,8 +221,8 @@ public class LoginController implements Initializable {
             System.out.println("New login recorded in log file.");
             writer.flush();
             writer.close();
-        } catch (IOException e) {
-            System.out.println(e);
+        } catch (IOException exception) {
+            exception.printStackTrace();
         }
     }
 
@@ -250,42 +230,29 @@ public class LoginController implements Initializable {
      * @param userID The userID of the person logging in.
      * @param password The password of the person logging in.
      * */
-    private boolean isValidPassword(int userID, String password) throws SQLException {
-        //create statement object
-        Statement statement = ConnectDB.conn.createStatement();
-
-        //write SQL statement
-        String sqlStatement = "SELECT password FROM users WHERE user_Id ='" + userID + "'";;
-
-        //create resultset object
-        ResultSet result = statement.executeQuery(sqlStatement);
-
-        while (result.next()) {
-            if (result.getString("password").equals(password)) {
+    public boolean passwordChecker(int userID, String password) throws SQLException {
+        PreparedStatement sql = ConnectDB.makeConnection().prepareStatement("SELECT password FROM users WHERE user_Id ='" + userID + "'");
+        ResultSet set = sql.executeQuery();
+        while (set.next()) {
+            if (set.getString("password").equals(password)) {
                 return true;
             }
         }
         return false;
     }
 
-    /** This method gets the User ID for current user.
+    /** This method obtains the User ID from who is logging in.
      * @param username The username of the person logging in.
      * */
-    private int getUserID(String username) throws SQLException {
-        int userID = -1;
+    public int getCreatorNumber(String username) throws SQLException {
+        int creatorNumber = -1;
 
-        //create statement object
-        Statement statement = ConnectDB.conn.createStatement();
-
-        //write SQL statement
-        String sqlStatement = "SELECT user_ID FROM users WHERE user_Name ='" + username + "'";
-
-        //create resultset object
-        ResultSet result = statement.executeQuery(sqlStatement);
-
-        while (result.next()) {
-            userID = result.getInt("user_Id");
+        PreparedStatement sql = ConnectDB.makeConnection().prepareStatement("\"SELECT user_ID FROM users WHERE " +
+                        "user_Name =" + username + "'");
+        ResultSet set = sql.executeQuery();
+        while (set.next()) {
+            creatorNumber = set.getInt("user_Id");
         }
-        return userID;
+        return creatorNumber;
     }
 }
